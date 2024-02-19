87aa32cca9897843dcad622d314da36c39c6eafc
Luis <luis.nunes@alunos.ifsuldeminas.edu.br>
2024-02-19 10:13:50 -0300

    Aula da semana 1

---
+++ Semana1/exemplo1_saida.py
@@ -0,0 +1,2 @@
+print("Olá", "3info A", sep="_", end=" ---> ")
+print("Sejam bem vindos!", sep="_")
---
+++ Semana1/exemplo2_entrada.py
@@ -0,0 +1,3 @@
+n1 = input("Digite o número 1: ")
+n2 = input("Digite o número 2: ")
+print("você digitou", n1, n2)
